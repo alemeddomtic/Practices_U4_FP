6a4646d1ef30573093ec0951f9550677d5bce7a7
alemeddomtic <143725975+alemeddomtic@users.noreply.github.com>
2023-10-19 09:45:46 -0500

    Add files via upload

---
+++ P_Cosas_Favoritas.java
@@ -0,0 +1,38 @@
+
+public class P_Cosas_Favoritas {
+    public static void main(String[] args) {
+       
+        //Cosas favoritas de Alexander M. D.
+        
+       String[] cfav = new String [11];
+        System.out.println("Mis cosas favoritas son las siguientes:");
+        
+       cfav [0]= "helado";
+       cfav [1]= "perros";
+       cfav [2]= "atole de chocolate";
+       cfav [3]= "enchiladas";
+       cfav [4]= "tacos dorados";
+       cfav [5]= "yoga";
+       cfav [6]= "programacion";
+       cfav [7]= "libros";
+       cfav [8]= "peliculas de terror";
+       cfav [9]= "cafe";
+       cfav [10]= "plumones";
+       
+        System.out.println("1. " + cfav [0]);
+        System.out.println("2. " + cfav [1]);
+        System.out.println("3. " + cfav [2]);
+        System.out.println("4. " + cfav [3]);
+        System.out.println("5. " + cfav [4]);
+        System.out.println("6. " + cfav [5]);
+        System.out.println("7. " + cfav [6]);
+        System.out.println("8. " + cfav [7]);
+        System.out.println("9. " + cfav [8]);
+        System.out.println("10. " + cfav [9]);
+        System.out.println("11. " + cfav [10]);
+        
+        //tqm
+        
+    }
+    
+}
